a26aec732f1c2eda4a66f62a2fc0c338c45bab0c
Leo <turniptulip@protonmail.com>
2026-06-21 02:08:12 -0300

    Added first exercise

---
+++ PY4E/ex_1.1_hello_world.py
@@ -0,0 +1,5 @@
+# First lesson of the Python for Everybody course, not much to say here the provided code was:
+# prinq("hello world")
+# the assignment being to fix the typo to make the command run.
+
+print("hello world")
